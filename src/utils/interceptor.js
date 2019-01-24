050872f944bbe68f345d4137e0b385e288097203
xy <3311599023@qq.com>
2019-01-24 13:02:16 +0800

    修正远程跨域问题

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -5,21 +5,19 @@ axios.defaults.withCredentials = true
 
 // //设置默认请求头
 axios.defaults.headers = {
-	'content-type' : 'application/json',  //传出类型 ）json,form表单
-	'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-
+	'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Access-Control-Allow-Origin': '*'
 }
 
-//请求响应 Toast.loading('加载中...',0);
+
+//请求响应
 axios.interceptors.request.use(
-	function (config) {
-    // Do something before request is sent
-    return config;
-  }, function (error) {
-    // Do something with request error
-     // setTimeoute()
+	 config=> {
+         Toast.loading('加载中...',0);
+        return config;
+  },  err=> {
      Toast.hide();
-    return Promise.reject(error);
+    return Promise.reject(err);
 })
 //返回响应
 axios.interceptors.response.use((data)=>{
